86f8738ea5234bccbc74da06cfb96fbd876d87fd
Dhairya Chheda <dhairyakchheda@gmail.com>
2023-12-23 19:49:33 +0530

    Update and delete book routes

--- backend/routes/bookRoutes.js
+++ backend/routes/bookRoutes.js
@@ -17,7 +17,7 @@ router.post("/", async (req, res) => {
     const book = await Book.create(newBook);
     res.status(201).json({ message: "Book added to the store!" });
   } catch (error) {
-    return res.status(400).json({ error: error.message });
+    return res.status(500).json({ error: error.message });
   }
 });
 
@@ -27,7 +27,7 @@ router.get("/", async (req, res) => {
     const books = await Book.find();
     return res.json(books);
   } catch (error) {
-    return res.status(400).json({ error: error.message });
+    return res.status(500).json({ error: error.message });
   }
 });
 
@@ -38,7 +38,33 @@ router.get("/:id", async (req, res) => {
     const book = await Book.findById(id);
     return res.json(book);
   } catch (error) {
-    return res.status(400).json({ error: error.message });
+    return res.status(500).json({ error: error.message });
+  }
+});
+
+// Update a book
+router.put("/:id", async (req, res) => {
+  try {
+    if (!req.body.title || !req.body.author || !req.body.publishYear)
+      return res.status(400).json({ message: "Please enter all fields." });
+    const { id } = req.params;
+    const book = await Book.findByIdAndUpdate(id, req.body);
+    if (!book) return res.status(404).json({ message: "Book not found" });
+    // res.json(book);
+    return res.status(200).json({ message: "Book updated successfully" });
+  } catch (error) {
+    return res.status(500).json({ error: error.message });
+  }
+});
+
+// Delete a book
+router.delete("/:id", async (req, res) => {
+  try {
+    const result = await Book.findByIdAndDelete(req.params.id);
+    if (!result) return res.status(404).json({ message: "Book not found" });
+    return res.status(200).json({ message: "Book is deleted" });
+  } catch (error) {
+    return res.status(500).json({ error: error.message });
   }
 });
 
